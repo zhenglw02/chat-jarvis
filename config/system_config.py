3f0e985a4b0c7c1c2938c95d8d485250ecfb62c0
zhenglw02 <liwei_zheng02@gmail.com>
2023-07-20 20:50:57 +0800

    gpt3.5的大脑支持流式读取返回内容，持续说话；大脑接口更好的抽象

--- config/system_config.py
+++ config/system_config.py
@@ -3,7 +3,7 @@ AI_NAME = "贾维斯"
 
 # 耳朵相关配置
 # 聆听一段话时，等待多长时间没有声音则认为已说完，单位秒
-PER_REQUEST_THRESHOLD_IN_SECOND = 1.5
+PER_REQUEST_THRESHOLD_IN_SECOND = 1.0
 
 # 嘴巴相关配置
 # 音量大小，取值为0.0 ～ 1.0
@@ -21,6 +21,8 @@ OPENAI_API_KEY = "sk-xxx"
 # 记忆相关配置
 # 最多保存多长的记忆
 MEMORY_MAX_LENGTH = 10
+# 流式获取大脑返回的语音内容时，等待获取到多长的内容时开始说话。
+START_SPEAK_CONTENT_LENGTH = 20
 
 # 写代码插件相关配置
 GENERATE_CODE_OPENAI_API_KEY = "sk-xxx"
---
+++ modules/brain/util.py
@@ -0,0 +1,6 @@
+def is_break_char(content):
+    break_chars = [",", "，", ".", "。", "?", "？", "!", "！", ":", "：", ";", "；"]
+    for c in break_chars:
+        if c in content:
+            return True
+    return False
