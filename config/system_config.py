0aca3be010bb43384e220e59d26865278692bec8
zhenglw02 <liwei_zheng02@gmail.com>
2023-07-28 22:02:25 +0800

    代码重构

--- config/system_config.py
+++ config/system_config.py
@@ -12,22 +12,22 @@ LOG_LEVEL = logging.DEBUG
 
 # 耳朵相关配置
 # 百度应用ID
-BAIDU_EAR_APP_ID = 36167526
+BAIDU_EAR_APP_ID = 0
 # 百度语音识别地址，可以改为wss://
 BAIDU_EAR_ASR_URI = "ws://vop.baidu.com/realtime_asr"
 # 百度语言识别模型，可以修改为其它语言模型测试，如远场普通话19362
 BAIDU_EAR_DEV_PID = 15372
 # 百度鉴权信息
-BAIDU_EAR_API_KEY = "vI2sOGUTNgphAvXACopM1i9a"
-BAIDU_EAR_SECRET_KEY = "8CFuL8vGtmIPuHziij50LSenf9ALBxqX"
+BAIDU_EAR_API_KEY = "xxx"
+BAIDU_EAR_SECRET_KEY = "xxx"
 # 聆听一段话时，等待多长时间没有声音则认为已说完，单位秒
 BAIDU_EAR_PER_REQUEST_THRESHOLD_IN_SECOND = 1.0
 
 # 嘴巴相关配置
 BAIDU_MOUTH_APP_ID = "36167526"
 # 百度鉴权信息
-BAIDU_MOUTH_API_KEY = "vI2sOGUTNgphAvXACopM1i9a"
-BAIDU_MOUTH_SECRET_KEY = "8CFuL8vGtmIPuHziij50LSenf9ALBxqX"
+BAIDU_MOUTH_API_KEY = "xxx"
+BAIDU_MOUTH_SECRET_KEY = "xxx"
 # 音量大小，取值为0.0 ～ 1.0
 BAIDU_MOUTH_VOICE_VOLUME = 0.5
 # 说话语速，取值为0 ～ 9
@@ -38,7 +38,7 @@ BAIDU_MOUTH_SPEAK_PER = 3
 # 大脑相关配置
 # 使用的openai模型
 BRAIN_OPENAI_MODEL = "gpt-3.5-turbo"
-BRAIN_OPENAI_API_KEY = "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"
+BRAIN_OPENAI_API_KEY = "sk-xxx"
 BRAIN_OPENAI_SYSTEM_PROMPT = f"你是一个人工智能管家，你的名字是{AI_NAME}。你本质上是我编写的一个程序，但你在和我交流时表现为一个专业的管家。\n" \
                 f"你有耳朵，可以听到我说的话；你有嘴巴，可以说话；你有一个交互板，可以通过交互板获取我手动输入的文字；\n" \
                 f"你有很多额外能力可以通过函数调用的方式使用，你应该注重函数的使用。\n" \
@@ -53,9 +53,9 @@ START_SPEAK_CONTENT_LENGTH = 20
 MEMORY_MAX_LENGTH = 10
 
 # 写代码插件相关配置
-GENERATE_CODE_OPENAI_API_KEY = "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"
+GENERATE_CODE_OPENAI_API_KEY = "sk-xxx"
 GENERATE_CODE_OPENAI_MODEL = "gpt-3.5-turbo-0613"
 
 # 提取摘要插件相关配置
-SUMMARY_FILE_OPENAI_API_KEY = "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"
+SUMMARY_FILE_OPENAI_API_KEY = "sk-xxx"
 SUMMARY_FILE_OPENAI_MODEL = "gpt-3.5-turbo-16k"
